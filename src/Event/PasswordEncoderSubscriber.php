5b91aa8cfaaced50f75f809483646d4c69121609
Daniel Boetsch <dboetsch@eleven-labs.com>
2022-03-21 14:51:19 +0100

    Encode password by event subscriber

---
+++ src/Event/PasswordEncoderSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Event;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class PasswordEncoderSubscriber implements EventSubscriberInterface
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;        
+    }
+    
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['encodePassword', EventPriorities::PRE_WRITE],
+        ];
+    }
+
+    public function encodePassword(ViewEvent $event): void
+    {
+        $user = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if (!$user instanceof User || Request::METHOD_POST !== $method) {
+            return;
+        }
+
+        $hash = $this->passwordEncoder->encodePassword($user, $user->getPassword());
+        $user->setPassword($hash);
+    }
+}

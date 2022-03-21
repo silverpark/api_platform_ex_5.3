98393b558c4c04887c3af7f6dc7a0377bb23f233
Daniel Boetsch <dboetsch@eleven-labs.com>
2022-03-21 11:21:08 +0100

    Entity user add constraints

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,6 +10,8 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ApiResource(
@@ -17,6 +19,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
  * )
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(name="`user`")
+ * @UniqueEntity("email")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -31,6 +34,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @ORM\Column(type="string", length=180, unique=true)
      * @Groups({"users_read","customers_read"})
+     * @Assert\Email()
      */
     private $email;
 
@@ -43,6 +47,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
+     * @Assert\NotBlank()
+     * @Assert\Length(min=6)
      */
     private $password;
 
@@ -55,12 +61,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"users_read","customers_read"})
+     * @Assert\NotBlank()
+     * @Assert\Length(min=3,max=255)
      */
     private $firstName;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"users_read","customers_read"})
+     * @Assert\NotBlank()
+     * @Assert\Length(min=3,max=255)
      */
     private $lastName;
 
